d11de5c05c8392e93f6fbaa50934fd4e55cfd018
Dave Liepmann <dave.liepmann@gmail.com>
2020-04-20 14:29:59 +0200

    Slack: create linked channel flow

--- src/org/sparkboard/server/slack.clj
+++ src/org/sparkboard/server/slack.clj
@@ -47,19 +47,56 @@
 
 ;; TODO automatically add users
 
-(comment ;; Flow: new linked channel is created
-  (create-channel! "is-this-thing-on")
+(defn create-linked-channel! [channel-name]
+  ;; TODO determine channel naming scheme
+  (let [create-rsp (create-channel! channel-name)
+        chnnl-id (-> create-rsp :body
+                     (json/read-value (json/object-mapper {:decode-key-fn keyword}))
+                     :channel :id)]
+    ;; Add members of the Sparkboard project to the channel
+    (invite-to-channel! channel-name ;; TODO user name lookup
+                        ["FIXME"])
+    ;; "Pin a message to the top of the channel, TODO linking back to
+    ;; the Sparkboard project"
+    (let [msg-rsp (-> (web-api "/chat.postMessage"
+                               {:channel chnnl-id :text "FIXME"}
+                               (json/object-mapper {:decode-key-fn keyword}))
+                      :body
+                      (json/read-value (json/object-mapper {:decode-key-fn keyword})))]
+      (if (:ok msg-rsp)
+        (web-api "/pins.add" {:channel chnnl-id
+                              :timestamp (:ts msg-rsp)})))
+    (web-api "/conversations.setPurpose" {:purpose "FIXME (purpose)" :channel chnnl-id})
+    (web-api "/conversations.setTopic" {:topic "FIXME (topic)" :channel chnnl-id})))
 
-  ;; "Pin a message to the top of the channel, linking back to the
-  ;; Sparkboard project"
+(comment ;; Flow: new linked channel is created
+  ;; Create channel
+  (create-channel! "is-this-thing-on") ;; TODO determine channel naming scheme
+
+  ;; "Add existing members of the project to the channel"
+  (invite-to-channel! "is-this-thing-on"
+                      ;; TODO user name lookup
+                      ["dave.liepmann"])
   
+  ;; "Pin a message to the top of the channel, TODO linking back to the
+  ;; Sparkboard project"  
   (let [chnnl (channel-id "is-this-thing-on")
         msg-rsp (json/read-value (:body (web-api "/chat.postMessage"
-                                                 {:channel chnnl :text "it's as easy as"}))
+                                                 {:channel chnnl :text "FIXME"}))
                                  (json/object-mapper {:decode-key-fn keyword}))]
     (if (:ok msg-rsp)
       (web-api "/pins.add" {:channel chnnl
                             :timestamp (:ts msg-rsp)})))
+
+  ;; Set channel purpose/description
+  (web-api "/conversations.setPurpose" {:purpose "FIXME (purpose)"
+                                        :channel (channel-id "is-this-thing-on")})
+
+  ;; Set channel topic
+  (web-api "/conversations.setTopic" {:topic "FIXME (topic)"
+                                      :channel (channel-id "is-this-thing-on")})
+  
+  ;; TODO "Store the channel ID with the project"
   
   )
 
@@ -73,4 +110,3 @@
 
   ;; TODO delete/archive channel, for testing and clean-up
   )
-
