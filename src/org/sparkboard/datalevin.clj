46647f72ff19272a288451aca628062f339c5fe9
Dave Liepmann <dave.liepmann@gmail.com>
2023-01-04 13:58:26 +0100

    rich comment: org query

--- src/org/sparkboard/datalevin.clj
+++ src/org/sparkboard/datalevin.clj
@@ -15,15 +15,20 @@
  (dl/close conn)
  (dl/clear conn)
 
+
+ (->> (d/where [:org/id])
+      (map (d/pull '[*
+                     #_ {:board.settings/default-template [*]}
+                     {:entity/domain [*]}
+                     {:ts/created-by [*]}])))
+
+ 
  (->> (d/where [:board/id])
       (map (d/pull '[(:board/id :db/id true)
                      :board/title
                      {:project/_board [:project/title]}]))
       (drop 3)
       first)
-
-
-
 
 
  )
