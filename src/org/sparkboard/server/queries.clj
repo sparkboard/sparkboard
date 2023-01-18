0177f4aa68af8d73f44c1c72483500c045c433b7
Matthew Huebert <mhuebert@gmail.com>
2023-01-18 10:25:30 +0100

    re-db: use ordinary r/reaction instead of re-db.query/reaction
    This simplifies code with little downside. For testing we can use
    re-db.query/bound-reaction if needed.

--- src/org/sparkboard/server/queries.clj
+++ src/org/sparkboard/server/queries.clj
@@ -1,9 +1,8 @@
 (ns org.sparkboard.server.queries
   (:require [re-db.reactive :as r]
-            [re-db.query :as q]
             [re-db.read :as read]
             [re-db.sync :as sync]
-            [re-db.memo :refer [def-memo defn-memo]]
+            [re-db.memo :refer [defn-memo]]
             [org.sparkboard.datalevin :refer [conn]]
             [re-db.api :as db]))
 
@@ -12,54 +11,54 @@
        (read/handle-report! conn)))
 
 (defn-memo $org:index [_]
-  (q/reaction conn
-    (->> (db/where [:org/id])
-         (mapv (re-db.api/pull '[*])))))
+  (r/reaction
+   (->> (db/where [:org/id])
+        (mapv (re-db.api/pull '[*])))))
 
 (defn-memo $org:one [{:keys [org/id]}]
-  (q/reaction conn
-    (db/pull '[:org/title
-               {:board/_org [:ts/created-at
-                             :board/id
-                             :board/title]}
-               {:entity/domain [:domain/name]}]
-             [:org/id id])))
+  (r/reaction
+   (db/pull '[:org/title
+              {:board/_org [:ts/created-at
+                            :board/id
+                            :board/title]}
+              {:entity/domain [:domain/name]}]
+            [:org/id id])))
 
 (defn-memo $board:index [_route-params]
-  (q/reaction conn
-    (->> (db/where [:board/id])
-         (mapv (re-db.api/pull '[:board/title])))))
+  (r/reaction
+   (->> (db/where [:board/id])
+        (mapv (re-db.api/pull '[:board/title])))))
 
 (defn-memo $board:one [{:keys [board/id] :as _route-params}]
-  (q/reaction conn
-              (db/pull '[*
-                         :board/title
-                         :board.registration/open?
-                         :board/title
-                         {:project/_board [*]}
-                         {:board/org [:org/title :org/id]}
-                         {:member/_board [*]}
-                         {:entity/domain [:domain/name]}]
-                       [:board/id id])))
+  (r/reaction
+   (db/pull '[*
+              :board/title
+              :board.registration/open?
+              :board/title
+              {:project/_board [*]}
+              {:board/org [:org/title :org/id]}
+              {:member/_board [*]}
+              {:entity/domain [:domain/name]}]
+            [:board/id id])))
 
 (defn-memo $project:index [_route-params]
   ;; TODO
   ;; pagination
   ;; search queries
-  (q/reaction conn
-    (->> (db/where [:project/id])
-         (take 20)
-         (mapv (re-db.api/pull '[:project/title])))))
+  (r/reaction
+   (->> (db/where [:project/id])
+        (take 20)
+        (mapv (re-db.api/pull '[:project/title])))))
 
 (defn-memo $project:one [{:keys [project/id]}]
-  (q/reaction conn
-    (db/pull '[*] [:project/id id])))
+  (r/reaction
+   (db/pull '[*] [:project/id id])))
 
 (defn-memo $member:one [{:keys [member/id]}]
-  (q/reaction conn
-    (db/pull '[*
-               {:member/tags [*]}]
-             [:member/id id])))
+  (r/reaction
+   (db/pull '[*
+              {:member/tags [*]}]
+            [:member/id id])))
 
 (defonce !list (atom ()))
 
