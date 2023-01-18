9fd88a2363af983d29bf32927c7945f18d1cccf5
Matthew Huebert <mhuebert@gmail.com>
2023-01-18 10:44:41 +0100

    try-value

--- src/org/sparkboard/server/queries.clj
+++ src/org/sparkboard/server/queries.clj
@@ -11,54 +11,61 @@
        (read/handle-report! conn)))
 
 (defn-memo $org:index [_]
-  (sync/reaction
-   (->> (db/where [:org/id])
-        (mapv (re-db.api/pull '[*])))))
+  (r/reaction
+   (sync/try-value
+    (->> (db/where [:org/id])
+         (mapv (re-db.api/pull '[*]))))))
 
 (defn-memo $org:one [{:keys [org/id]}]
-  (sync/reaction
-   (db/pull '[:org/title
-              {:board/_org [:ts/created-at
-                            :board/id
-                            :board/title]}
-              {:entity/domain [:domain/name]}]
-            [:org/id id])))
+  (r/reaction
+   (sync/try-value
+    (db/pull '[:org/title
+               {:board/_org [:ts/created-at
+                             :board/id
+                             :board/title]}
+               {:entity/domain [:domain/name]}]
+             [:org/id id]))))
 
 (defn-memo $board:index [_route-params]
-  (sync/reaction
-   (->> (db/where [:board/id])
-        (mapv (re-db.api/pull '[:board/title])))))
+  (r/reaction
+   (sync/try-value
+    (->> (db/where [:board/id])
+         (mapv (re-db.api/pull '[:board/title]))))))
 
 (defn-memo $board:one [{:keys [board/id] :as _route-params}]
-  (sync/reaction
-   (db/pull '[*
-              :board/title
-              :board.registration/open?
-              :board/title
-              {:project/_board [*]}
-              {:board/org [:org/title :org/id]}
-              {:member/_board [*]}
-              {:entity/domain [:domain/name]}]
-            [:board/id id])))
+  (r/reaction
+   (sync/try-value
+    (db/pull '[*
+               :board/title
+               :board.registration/open?
+               :board/title
+               {:project/_board [*]}
+               {:board/org [:org/title :org/id]}
+               {:member/_board [*]}
+               {:entity/domain [:domain/name]}]
+             [:board/id id]))))
 
 (defn-memo $project:index [_route-params]
   ;; TODO
   ;; pagination
   ;; search queries
-  (sync/reaction
-   (->> (db/where [:project/id])
-        (take 20)
-        (mapv (re-db.api/pull '[:project/title])))))
+  (r/reaction
+   (sync/try-value
+    (->> (db/where [:project/id])
+         (take 20)
+         (mapv (re-db.api/pull '[:project/title]))))))
 
 (defn-memo $project:one [{:keys [project/id]}]
-  (sync/reaction
-   (db/pull '[*] [:project/id id])))
+  (r/reaction
+   (sync/try-value
+    (db/pull '[*] [:project/id id]))))
 
 (defn-memo $member:one [{:keys [member/id]}]
-  (sync/reaction
-   (db/pull '[*
-              {:member/tags [*]}]
-            [:member/id id])))
+  (r/reaction
+   (sync/try-value
+    (db/pull '[*
+               {:member/tags [*]}]
+             [:member/id id]))))
 
 (defonce !list (atom ()))
 
