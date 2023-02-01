3a890979e3b8e188077f750e4ad9e98e50d72206
Matthew Huebert <mhuebert@gmail.com>
2023-02-01 11:04:09 +0100

    defquery

--- src/org/sparkboard/server/queries.clj
+++ src/org/sparkboard/server/queries.clj
@@ -1,69 +1,70 @@
 (ns org.sparkboard.server.queries
   (:require [org.sparkboard.datalevin :as sb.datalevin :refer [conn]]
             [re-db.api :as db]
-            [re-db.memo :refer [defn-memo]]
+            [re-db.memo :as memo]
             [re-db.reactive :as r]
             [re-db.read :as read]))
+
+(defmacro defquery
+  "Defines a query function. The function will be memoized and return a {:value / :error} map."
+  [name & fn-body]
+  (let [[doc [argv & body]] (if (string? (first fn-body))
+                              [(first fn-body) (rest fn-body)]
+                              [nil fn-body])]
+    `(memo/defn-memo ~name ~argv
+       (r/reaction ~@body))))
 
 (defn transact! [txs]
   (read/transact! conn txs))
 
-(defn-memo $org:index [_]
-  (r/reaction
-   (->> (db/where [:org/id])
-        (mapv (re-db.api/pull '[*])))))
+(defquery $org:index [_]
+  (->> (db/where [:org/id])
+       (mapv (re-db.api/pull '[*]))))
 
-(defn-memo $org:one [{:keys [org/id]}]
-  (r/reaction
-   (db/pull '[:org/id
-              :org/title
-              {:board/_org [:ts/created-at
-                            :board/id
-                            :board/title]}
-              {:entity/domain [:domain/name]}]
-            [:org/id id])))
+(defquery $org:one [{:keys [org/id]}]
+  (db/pull '[:org/id
+             :org/title
+             {:board/_org [:ts/created-at
+                           :board/id
+                           :board/title]}
+             {:entity/domain [:domain/name]}]
+           [:org/id id]))
 
-(defn-memo $board:index [_route-params]
-  (r/reaction
-   (->> (db/where [:board/id])
-        (mapv (re-db.api/pull '[:board/title])))))
+(defquery $board:index [_route-params]
+  (->> (db/where [:board/id])
+       (mapv (re-db.api/pull '[:board/title]))))
 
-(defn-memo $board:one [{:keys [board/id] :as _route-params}]
-  (r/reaction
-   (db/pull '[*
-              :board/title
-              :board.registration/open?
-              :board/title
-              {:project/_board [*]}
-              {:board/org [:org/title :org/id]}
-              {:member/_board [*]}
-              {:entity/domain [:domain/name]}]
-            [:board/id id])))
+(defquery $board:one [{:keys [board/id] :as _route-params}]
+  (db/pull '[*
+             :board/title
+             :board.registration/open?
+             :board/title
+             {:project/_board [*]}
+             {:board/org [:org/title :org/id]}
+             {:member/_board [*]}
+             {:entity/domain [:domain/name]}]
+           [:board/id id]))
 
-(defn-memo $project:index [_route-params]
-  (r/reaction
-   ;; TODO
-   ;; pagination
-   ;; search queries
-   (->> (db/where [:project/id])
-        (take 20)
-        (mapv (re-db.api/pull '[:project/title])))))
+(defquery $project:index [_route-params]
+  ;; TODO
+  ;; pagination
+  ;; search queries
+  (->> (db/where [:project/id])
+       (take 20)
+       (mapv (re-db.api/pull '[:project/title]))))
 
-(defn-memo $project:one [{:keys [project/id]}]
-  (r/reaction
-   (db/pull '[*] [:project/id id])))
+(defquery $project:one [{:keys [project/id]}]
+  (db/pull '[*] [:project/id id]))
 
-(defn-memo $member:one [{:keys [member/id]}]
-  (r/reaction
-   (db/pull '[*
-              {:member/tags [*]}]
-            [:member/id id])))
+(defquery $member:one [{:keys [member/id]}]
+  (db/pull '[*
+             {:member/tags [*]}]
+           [:member/id id]))
 
-(defn-memo $search [{:keys [query-params org/id] :as route-params}]
-  (r/reaction
-   (->> (sb.datalevin/q-fulltext-in-org (:q query-params)
-                                        id)
-        ;; Can't send Entities over the wire, so:
-        (map (db/pull '[:project/title
-                        :board/title])))))
+(defquery $search [{:keys [query-params org/id] :as route-params}]
+  (->> (sb.datalevin/q-fulltext-in-org (:q query-params)
+                                       id)
+       ;; Can't send Entities over the wire, so:
+       (map (db/pull '[:project/title
+                       :board/title]))))
 
