1fa5918f517fd5a2c70236445e3b39f90c292c60
Dave Liepmann <dave.liepmann@gmail.com>
2023-02-22 11:47:02 +0100

    confirmation before delete

--- src/org/sparkboard/client/views.cljs
+++ src/org/sparkboard/client/views.cljs
@@ -25,7 +25,8 @@
                  [rough/card {:class "pa3"}
                   [rough/link {:href (routes/path-for [:org/one org])}
                    (:org/title org)]
-                  [rough/icon-button {:on-click #(routes/mutate! {:route [:org/delete]} org)}
+                  [rough/icon-button {:on-click #(when (js/window.confirm (str "Really delete organization " (:org/title org) "?"))
+                                                   (routes/mutate! {:route [:org/delete]} org))}
                    "X"]]))
           (ws/use-query! [:org/index {}]))]
    [:section#add-org
