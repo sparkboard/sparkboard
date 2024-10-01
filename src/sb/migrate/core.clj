61d910302d125d4c9029bdbe31df5a9d73cd1049
Emanuel Rylke <ema@mailbox.org>
2024-10-01 20:33:23 +0200

    Speed up migration

--- src/sb/migrate/core.clj
+++ src/sb/migrate/core.clj
@@ -18,10 +18,20 @@
     ;; upsert lookup refs
     (u/timed `transact1 (db/transact! (mapcat sch/unique-keys entities)))
     ;; transact entities
-    (-> (u/timed `transact2 (db/transact! (one-time/all-entities)))
-        :tx-data
-        count
-        (str " datoms"))))
+    (u/timed `transact2
+             (let [entities (one-time/all-entities)
+                   ;; for some reason transacting in chunks is faster
+                   chunks (partition 1000 entities)]
+               (println (count entities) "entities")
+               (println (apply str (repeat (count chunks) "-")))
+               (doto (apply + (for [chunk chunks]
+                                (do (print ".")
+                                    (flush)
+                                    (-> (db/transact! chunk)
+                                        :tx-data
+                                        count))))
+                 ((fn [_] (prn)))
+                 (println "datoms"))))))
 
 (comment
   (tx!)
