e3dffe4271ee0cb8af9a53d495b6e68133d4207c
Emanuel Rylke <ema@mailbox.org>
2024-10-31 18:30:26 +0100

    remove roles from note memberships

--- src/sb/migrate/one_time.clj
+++ src/sb/migrate/one_time.clj
@@ -1115,12 +1115,9 @@
                                                                       (fn [{:as   member
                                                                             :keys [user_id]}]
                                                                         (when-let [account-id (member->account-uuid user_id)]
-                                                                          ;; TODO review if note roles are actually used / make sense
-                                                                          (let [role (some->> (:role member) (role-kw :note))]
-                                                                            (merge {:entity/id         (composite-uuid :membership note-id account-id)
-                                                                                    :entity/kind       :membership
-                                                                                    :membership/member (uuid-ref :account account-id)}
-                                                                                   (when role {:membership/roles #{role}})))))
+                                                                          {:entity/id         (composite-uuid :membership note-id account-id)
+                                                                           :entity/kind       :membership
+                                                                           :membership/member (uuid-ref :account account-id)}))
                                                                      v))))
                                                   (rename :membership/_entity))
 
