b34c01658100a34078886f9a0f81e638ded8d430
Emanuel Rylke <ema@mailbox.org>
2024-11-26 21:45:25 +0100

    Fix test-new-message!

--- test/sb/app/chat/data_test.clj
+++ test/sb/app/chat/data_test.clj
@@ -1,12 +1,14 @@
 (ns sb.app.chat.data-test
   (:require [clojure.test :refer [deftest testing is]]
             [sb.app.chat.data :as cd]
+            [sb.app.content.data] ;; needed for schema definition
+            [sb.app.notification.data]  ;; needed for schema definition
             [re-db.api :as db]
             [sb.authorize :as az]
             [sb.schema :as sch]
-            [sb.server.datalevin :as dl]
-            ;; TODO need to require this for the schema to be properly defined, but is slow
-            [sb.app]))
+            [sb.server.datalevin :as dl]))
+
+(sch/install-malli-schemas!)
 
 (defn get-test-db-path []
   ;; Datalevin doesn't have an in-memory mode therefore we create a test database in tmpdir
@@ -16,6 +18,10 @@
   (str (System/getProperty "java.io.tmpdir")
        "/sparkboard-test-" (System/currentTimeMillis) "-" (rand-int 10000000)))
 
+(defn prose [content]
+  {:prose/format :prose.format/markdown
+   :prose/string content})
+
 (deftest test-new-message!
   (let [conn (datalevin.core/get-conn (get-test-db-path) {} dl/db-opts)]
     (db/with-conn conn
@@ -23,53 +29,41 @@
         ;; transact schema
         (db/merge-schema! @sch/!schema)
 
-        (let [tx-report (db/transact! [(dl/new-entity {:db/id "new-board"} :board)
-                                       (dl/new-entity {:db/id "alice-account"} :account)
-                                       (dl/new-entity {:db/id "bob-account" :account/email "bob"} :account)
-                                       (dl/new-entity {:db/id "eve-account" :account/email "eve"} :account)
-                                       (dl/new-entity {:db/id "alice" :membership/entity "new-board"
-                                                       :membership/member "alice-account"}
-                                                      :membership)
-                                       (dl/new-entity {:db/id "bob" :membership/entity "new-board"
-                                                       :membership/member "bob-account"}
-                                                      :membership)
-                                       (dl/new-entity {:db/id "eve" :membership/entity "new-board"
-                                                       :membership/member "eve-account"}
-                                                      :membership)])
-              {:strs [new-board alice alice-account bob bob-account eve eve-account]} (-> tx-report :tempids)
-              chat-id1 (:chat-id (cd/new-message! {:account-id (sch/wrap-id (db/entity alice-account))
+        (let [tx-report (db/transact! [(dl/new-entity {:db/id "alice"} :account)
+                                       (dl/new-entity {:db/id "bob" :account/email "bob"} :account)
+                                       (dl/new-entity {:db/id "eve" :account/email "eve"} :account)])
+              {:strs [alice bob eve]} (-> tx-report :tempids)
+              chat-id1 (:chat-id (cd/new-message! {:account-id (sch/wrap-id (db/entity alice))
                                                    :other-id (sch/wrap-id (db/entity bob))}
-                                                  "hello1"))
-              chat-id2 (:chat-id (cd/new-message! {:account-id (sch/wrap-id (db/entity alice-account))
+                                                  (prose "hello1")))
+              chat-id2 (:chat-id (cd/new-message! {:account-id (sch/wrap-id (db/entity alice))
                                                    :other-id (sch/wrap-id (db/entity bob))}
-                                                  "hello2"))
+                                                  (prose "hello2")))
               alice-id (:entity/id (db/entity alice))
               bob-id (:entity/id (db/entity bob))]
           (testing "two messages from alice to bob end up in the same chat"
             (is (= chat-id1 chat-id2)))
           (testing "bob can reply"
-            (cd/new-message! {:account-id (sch/wrap-id (db/entity bob-account))
+            (cd/new-message! {:account-id (sch/wrap-id (db/entity bob))
                               :other-id (sch/wrap-id (db/entity alice))}
-                             "hello3"))
+                             (prose "hello3")))
           (testing "alice and bob can read the chat"
-            (is (= [[alice-id "hello1"]
-                    [alice-id "hello2"]
-                    [bob-id "hello3"]]
-                   (->> (cd/chat (az/with-account-id! {:account (db/entity alice-account)}
+            (is (= [[alice-id (prose "hello1")]
+                    [alice-id (prose "hello2")]
+                    [bob-id (prose "hello3")]]
+                   (->> (cd/chat (az/with-account-id! {:account (db/entity alice)}
                                    {:other-id (sch/wrap-id (db/entity bob))}))
-                        :chat/messages
                         (map (juxt (comp :entity/id :entity/created-by) :chat.message/content)))
-                   (->> (cd/chat (az/with-account-id! {:account (db/entity bob-account)}
+                   (->> (cd/chat (az/with-account-id! {:account (db/entity bob)}
                                    {:other-id (sch/wrap-id (db/entity alice))}))
-                        :chat/messages
                         (map (juxt (comp :entity/id :entity/created-by) :chat.message/content))))))
           (testing "eve cannot read the the chat"
             (is (= []
-                   (->> (cd/chat (az/with-account-id! {:account (db/entity eve-account)}
+                   (->> (cd/chat (az/with-account-id! {:account (db/entity eve)}
                                    {:other-id (sch/wrap-id (db/entity bob))}))
                         :chat/messages
                         (map (juxt (comp :entity/id :entity/created-by) :chat.message/content)))
-                   (->> (cd/chat (az/with-account-id! {:account (db/entity eve-account)}
+                   (->> (cd/chat (az/with-account-id! {:account (db/entity eve)}
                                    {:other-id (sch/wrap-id (db/entity alice))}))
                         :chat/messages
                         (map (juxt (comp :entity/id :entity/created-by) :chat.message/content)))))))))))
