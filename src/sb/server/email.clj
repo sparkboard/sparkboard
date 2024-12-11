5312e2bf0f715f3d61de949e810ca8f35d4efaa2
Emanuel Rylke <er@redsky.io>
2024-12-11 10:40:16 +0100

    Send emails with AWS SES

--- src/sb/server/email.clj
+++ src/sb/server/email.clj
@@ -1,9 +1,31 @@
-(ns sb.server.email)
+(ns sb.server.email
+  (:require [cognitect.aws.client.api :as aws]
+            [cognitect.aws.credentials :as credentials]
+            [sb.server.env :as env]))
 
-(defn send! [{:keys [to subject body]}]
-  (println "Would send email to:" to)
-  (println "Subject:" subject)
-  (println body))
+(def ses (aws/client {:api :email
+                      :region (env/config :aws.ses/region)
+                      :credentials-provider
+                      (credentials/basic-credentials-provider
+                       {:access-key-id (env/config :aws.ses/access-key-id)
+                        :secret-access-key (env/config :aws.ses/secret-access-key)})}))
+
+(aws/validate-requests ses true)
+
+(defn aws-send! [{:keys [to subject body]}]
+  (aws/invoke ses {:op :SendEmail
+                   :request {:Source (env/config :sparkbot/email)
+                             :Destination {:ToAddresses [to]}
+                             :Message {:Subject {:Data subject}
+                                       :Body {:Text {:Data body}}}}}))
+
+(defn send! [{:as params :keys [to subject body]}]
+  (if (= "dev" (:env env/config))
+    (do
+      (println "Would send email to:" to)
+      (println "Subject:" subject)
+      (println body))
+    (aws-send! params)))
 
 
 (comment
